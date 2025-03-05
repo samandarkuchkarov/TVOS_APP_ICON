97772f25dc9fabee9b3328a4bc40c147b993cff7
samandar <samandar8939522@gmail.com>
2025-03-05 12:50:17 +0500

    add TVFocusGuideView

--- App.tsx
+++ App.tsx
@@ -5,31 +5,39 @@
  * @format
  */
 
-import React from 'react';
+import React, {useRef} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  findNodeHandle,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
+  TVFocusGuideView,
   useColorScheme,
   useWindowDimensions,
   View,
 } from 'react-native';
 
 function App(): React.JSX.Element {
-  const halfItems = Array.from(Array(5).keys());
+  const halfItems = Array.from(Array(4).keys());
   const fullItems = Array.from(Array(10).keys());
   const {width} = useWindowDimensions();
+  const ref = useRef<View>(null);
+  const [nextFocusDownId, setNextFocusDownId] = React.useState<number>();
 
   return (
     <View style={styles.container}>
-      <View style={[styles.list, {justifyContent: 'flex-end'}]}>
+      <TVFocusGuideView
+        autoFocus
+        style={[styles.list, {justifyContent: 'flex-end'}]}>
         {halfItems.map(i => {
           return (
             <TouchableOpacity
+              //@ts-ignore
+              nextFocusDown={nextFocusDownId}
               key={i}
               style={{
                 width: width / 10 - 20,
@@ -39,12 +47,18 @@ function App(): React.JSX.Element {
             />
           );
         })}
-      </View>
-      <View style={styles.list}>
+      </TVFocusGuideView>
+      <TVFocusGuideView autoFocus style={[styles.list, {width: 1000}]}>
         {halfItems.map(i => {
           return (
             <TouchableOpacity
               key={i}
+              ref={r => {
+                let id = findNodeHandle(r);
+                if (id) {
+                  setNextFocusDownId(id);
+                }
+              }}
               style={{
                 width: width / 10 - 20,
                 height: 60,
@@ -53,8 +67,8 @@ function App(): React.JSX.Element {
             />
           );
         })}
-      </View>
-      <View style={styles.list}>
+      </TVFocusGuideView>
+      <TVFocusGuideView autoFocus style={styles.list}>
         {fullItems.map(i => {
           return (
             <TouchableOpacity
@@ -67,7 +81,7 @@ function App(): React.JSX.Element {
             />
           );
         })}
-      </View>
+      </TVFocusGuideView>
     </View>
   );
 }

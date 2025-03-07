f24e911c7464315e9a6c201e3d51fa33be2b44e4
samandar <samandar8939522@gmail.com>
2025-03-07 10:01:13 +0500

    native_component

--- App.tsx
+++ App.tsx
@@ -5,76 +5,14 @@
  * @format
  */
 
-import React, {useRef} from 'react';
-import {
-  FocusDestination,
-  StyleSheet,
-  TouchableOpacity,
-  TVFocusGuideView,
-  useWindowDimensions,
-  View,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import MyNativeView from './MyNativeView';
 
 function App(): React.JSX.Element {
-  const halfItems = Array.from(Array(4).keys());
-  const fullItems = Array.from(Array(10).keys());
-  const {width} = useWindowDimensions();
-  const [nextFocusDown, setNextFocusDown] = React.useState<FocusDestination>();
-  const nextFocusDownRef = useRef<FocusDestination>(null);
-
   return (
     <View style={styles.container}>
-      <TVFocusGuideView
-        autoFocus
-        style={[styles.list, {justifyContent: 'flex-end'}]}>
-        {halfItems.map(i => {
-          return (
-            <TouchableOpacity
-              nextFocusDown={i == 0 ? nextFocusDown : nextFocusDownRef.current}
-              key={i}
-              style={{
-                width: width / 10 - 20,
-                height: 60,
-                backgroundColor: 'red',
-              }}
-            />
-          );
-        })}
-      </TVFocusGuideView>
-      <TVFocusGuideView autoFocus style={[styles.list, {width: 1000}]}>
-        {halfItems.map(i => {
-          return (
-            <TouchableOpacity
-              key={i}
-              ref={r => {
-                if (i == 0 && r) {
-                  nextFocusDownRef.current = r;
-                  setNextFocusDown(r);
-                }
-              }}
-              style={{
-                width: width / 10 - 20,
-                height: 60,
-                backgroundColor: 'red',
-              }}
-            />
-          );
-        })}
-      </TVFocusGuideView>
-      <TVFocusGuideView autoFocus style={styles.list}>
-        {fullItems.map(i => {
-          return (
-            <TouchableOpacity
-              key={i}
-              style={{
-                width: width / 10 - 20,
-                height: 60,
-                backgroundColor: 'red',
-              }}
-            />
-          );
-        })}
-      </TVFocusGuideView>
+      <MyNativeView style={{width: 100, height: 100, backgroundColor: 'red'}} />
     </View>
   );
 }
---
+++ MyNativeView.ts
@@ -0,0 +1,5 @@
+import {requireNativeComponent} from 'react-native';
+
+const MyNativeView = requireNativeComponent('MyNativeView');
+
+export default MyNativeView;
